f7bb576e278926dc6467619f3da31afab98dcd47
Julian Williams <julianthedev@gmail.com>
2019-04-24 09:11:12 -0700

    formatting

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,13 +6,11 @@ class Quiz extends Component {
     super(props)
     this.state={quiz_position: 1}
   }
-
   render(){
     return(
-       <div><div className="QuizQuestion"> {quizData.quiz_questions[0].instruction_text}</div></div>
+       <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
     )
   }
-
 }
 
 export default Quiz
