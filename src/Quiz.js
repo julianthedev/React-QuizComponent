bf44fc4905ed930c0e1b6fb937fd03fccb1f0af5
Julian Williams <julianthedev@gmail.com>
2019-04-25 14:53:17 -0700

    Added event handlers to components and logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,6 +8,11 @@ class Quiz extends Component {
     super(props);
     this.state = { quiz_position: 1 };
   }
+  showNextQuestion() {
+    this.setState(state => {
+      return { quiz_position: state.quiz_position + 1 };
+    });
+  }
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -17,6 +22,7 @@ class Quiz extends Component {
           <QuizEnd />
         ) : (
           <QuizQuestion
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
