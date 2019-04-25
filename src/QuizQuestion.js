8ea9ce45d792ecdd0a4049f861051b1cd6df0e15
Julian Williams <julianthedev@gmail.com>
2019-04-25 16:44:04 -0700

    add logic for incorrect answer.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,14 +2,27 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton.js";
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { incorrectAnswer: false };
+  }
   handleClick(buttonText) {
-    buttonText === this.props.quiz_question.answer &&
+    // buttonText === this.props.quiz_question.answer &&
+    //   this.props.showNextQuestionHandler();
+    if (buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false });
       this.props.showNextQuestionHandler();
+    } else {
+      this.setState({ incorrectAnswer: true });
+    }
   }
   render() {
     return (
       <main>
         <section>
+          {this.state.incorrectAnswer ? (
+            <p className="error">Incorrect answer. You're an idiot.</p>
+          ) : null}
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
