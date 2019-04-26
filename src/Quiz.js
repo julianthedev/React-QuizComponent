6f8cb39a7814b6373c0267b52d0d77708c0a2cee
Julian Williams <julianthedev@gmail.com>
2019-04-26 12:06:12 -0700

    Added event handlers for restarting quiz once finished.

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,13 +13,18 @@ class Quiz extends Component {
       return { quiz_position: state.quiz_position + 1 };
     });
   }
+  handleResetClick() {
+    this.setState(state => {
+      return { quiz_position: 1 };
+    });
+  }
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             showNextQuestionHandler={this.showNextQuestion.bind(this)}
